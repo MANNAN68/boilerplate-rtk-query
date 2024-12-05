48ee99fb4b726869d19faeec6707bc86a095cfa2
mannanswe <147591955+mannanswe@users.noreply.github.com>
2024-12-05 17:20:08 +0600

    complete full authentication process like register and login

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import useAuthCheck from "./hooks/useAuthCheck";
+import PublicRoute from "./route/PublicRoute";
+import PrivateRoute from "./route/PrivateRoute";
 
 function App() {
-    return (
+    const authCheck = useAuthCheck();
+
+    return !authCheck ? (<div>Checking authentication....</div>) : (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<PublicRoute><Login /></PublicRoute>} />
+                <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
+                <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
             </Routes>
         </Router>
-    );
+    )
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from '../components/header/Navbar'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <Navbar />
+            <div className="max-w-7xl mx-auto mt-2">
+                <div className="min-w-full border rounded flex lg:grid lg:grid-cols-3">
+                    Well come home page
+                </div>
+            </div>
+
+        </>
     )
 }
 
